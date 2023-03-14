3f671344d0657b68729e490d49def5c75144ace9
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2023-03-14 11:22:32 +0200

    sends acks to sender

--- receiver.py
+++ receiver.py
@@ -2,8 +2,10 @@ import select
 import socket
 from ftplib import FTP
 
+import packet
+
 RUDP_PORT = 5000  # Define the RUDP port number to use
-RUDP_IP = '127.0.0.1'  # Define the RUDP HOST number to use
+RUDP_HOST = '127.0.0.1'  # Define the RUDP HOST number to use
 FTP_PORT = 21  # Define the FTP port number to use
 BUFFER_SIZE = 1024  # Define the buffer size for data transfer
 MAX_RETRIES = 5  # Define the maximum number of RUDP transfer retries
@@ -32,7 +34,7 @@ def tcp_download(file_name):
 
 
 def start_tcp_connection():
-    username = 'Sagitush'  #input("User name: ")
+    username = 'Sagit'  #input("User name: ")
     password = '123456'  #input("Password: ")
 
     ftp = FTP()
@@ -42,71 +44,122 @@ def start_tcp_connection():
     return ftp
 
 
+def foo():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((RUDP_HOST, RUDP_PORT))
+    ftp_socket.send("rudp socket is up!".encode())
+    while True:
+        ans, addr = sock.recvfrom(1024)
+        print(ans)
+        sock.sendto("ack".encode(), addr)
+
+
+def open_file_to_write(file_name):
+    try:
+        file = open(file_name, 'wb')
+        return file
+    except IOError:
+        print('filed open file', file_name)
+        return
+
+
 def rudp_download(file_name):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((RUDP_IP, RUDP_PORT))
+    sock.bind((RUDP_HOST, RUDP_PORT))
 
     ftp_socket.send("rudp socket is up!".encode())
+    file = open_file_to_write(file_name)
+    pak, addr = sock.recvfrom(1024)
 
-    data_recv = False
-
+    expected_seq_num = 0
     while True:
-        file = open(file_name, 'wb')
-        timeout = 10
-        ctr = 0
-        while True:
-            ready = select.select([sock], [], [], timeout)
-            if ready[0]:
-                pak, addr = sock.recvfrom(1024)
-                file.write(pak)
-                # sock.sendto("ack".encode(), (RUDP_IP, RUDP_PORT))
-            else:
-                print(f"{file_name} received successfully!")
-                file.close()
-                data_recv = True
-                break
+        # get the next packet from sender
+        pak, addr = sock.recvfrom(1024)
 
-        if data_recv:
+        if not pak:
             break
 
+        seq_num, data = packet.extract(pak)
+        print('got packet num ', seq_num)
+
+        # send ACK to server
+        if seq_num == expected_seq_num:
+            print('got expected packet, sending ACK ', expected_seq_num)
+            pak = packet.make(expected_seq_num)
+            sock.sendto(pak, addr)
+            expected_seq_num += 1
+            file.write(data)
+        else:
+            print('sending ACK', expected_seq_num - 1)
+            pak = packet.make(expected_seq_num - 1)
+            sock.sendto(pak, addr)
     file.close()
     sock.close()
+
+
+
+    ###########
+    # while True:
+    #     file = open(file_name, 'wb')
+    #     timeout = 10
+    #     ctr = 0
+    #     while True:
+    #         ready = select.select([sock], [], [], timeout)
+    #         if ready[0]:
+    #             pak, addr = sock.recvfrom(1024)
+    #             file.write(pak)
+    #             # sock.sendto("ack".encode(), (RUDP_IP, RUDP_PORT))
+    #         else:
+    #             print(f"{file_name} received successfully!")
+    #             file.close()
+    #             data_recv = True
+    #             break
+    #
+    #     if data_recv:
+    #         break
+    #
+    # file.close()
+    # sock.close()
 
 
 def ask_server():
     # Send FTP commands to server
     while True:
         command = input("> ")
-
-        ftp_socket.send(command.encode())
-
-        if command == "QUIT":
-            # Exit loop and close FTP connection
-            ftp_socket.close()
-            break
-
-        elif command.startswith("GET"):
-            if command.split()[1] == "--tcp":
-                filename = command.split()[2]
-                tcp_download(filename)
-            else:
-                filename = command.split()[1]
-                rudp_download(filename)
-
-        elif command == "LIST":
-            # Receive a list of files in the current directory from server
-            data = ftp_socket.recv(BUFFER_SIZE)
-            print(data.decode())
-
-        elif command.startswith("UPLOAD"):
-            filename = command.split()[1]
-            tcp_upload(filename)
-
+        if command == "a":
+            ftp_socket.send("GET filee.txt".encode())
+            rudp_download('filee.txt')
+            # foo()
         else:
-            print("unknown command.")
-            # Receive FTP command response from server
-            data = ftp_socket.recv(BUFFER_SIZE)
-            print(data.decode())
+            ftp_socket.send(command.encode())
+
+            if command == "QUIT":
+                # Exit loop and close FTP connection
+                ftp_socket.close()
+                break
+
+            elif command.startswith("GET"):
+                if command.split()[1] == "--tcp":
+                    filename = command.split()[2]
+                    tcp_download(filename)
+                else:
+                    filename = command.split()[1]
+                    rudp_download(filename)
+
+            elif command == "LIST":
+                # Receive a list of files in the current directory from server
+                data = ftp_socket.recv(BUFFER_SIZE)
+                print(data.decode())
+
+            elif command.startswith("UPLOAD"):
+                filename = command.split()[1]
+                tcp_upload(filename)
+
+            else:
+                print("unknown command.")
+                # Receive FTP command response from server
+                data = ftp_socket.recv(BUFFER_SIZE)
+                print(data.decode())
 
 
 def connect_to_server():
@@ -119,10 +172,9 @@ def connect_to_server():
 if __name__ == '__main__':
 
     ftp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create an FTP socket
-    ftp_socket.connect(('localhost', FTP_PORT))  # Connect to the FTP server
+    ftp_socket.connect(('127.0.0.1', FTP_PORT))  # Connect to the FTP server
 
     connect_to_server()
 
     # Close the FTP connection
     ftp_socket.close()
-
