2c0408c96387684e720757fb61bf1b04f2a3e22d
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2023-03-14 16:52:15 +0200

    3 way handshake error

--- sender.py
+++ sender.py
@@ -20,10 +20,11 @@ from timer import Timer
 RUDP_PORT = 5000
 RUDP_HOST = '127.0.0.1'
 FTP_PORT = 21
+MTU = 1500
 PACKET_SIZE = 1024
 SLEEP = 0.1
-TIMEOUT_INTERVAL = 0.5
-WINDOW_SIZE = 4
+TIMEOUT = 10   # sec
+WINDOW_SIZE = 1
 
 # TCP configuration
 TCP_HOST = '0.0.0.0'
@@ -39,14 +40,69 @@ base = 0
 mutex = _thread.allocate_lock()
 send_timer = Timer()
 
+def handshake(conn):
+    step1 = False
+    message = ''
+    addr = ''
 
-# set window size
-def set_window_size(num_packets):
-    global base
-    return min(WINDOW_SIZE, num_packets - base)
+    for i in range (5):
+        try:
+            temp1, temp2 = receive_ack(conn)
+            if temp1 == 'TIMEOUT':
+                continue
+            if not step1:
+                message = temp1
+                addr = temp2
+                step1 = True
+        except IOError:
+          continue
+
+    if message == '' or addr == '':
+        print('packet loss ')
+        return False
+
+    if message[0] == 'HandShake':
+
+        for k in range (5):
+            conn.sendto('ACK'.encode(), addr)
+
+        time.sleep(1)
+
+        print('rudp connection established')
+
+        response = ''
+        addr = ''
+        step3 = False
+        for i in range(5):
+            try:
+                temp1, temp2 = receive_ack(conn)
+                if temp1 == 'TIMEOUT':
+                    continue
+                if not step3:
+                    response = temp1
+                    addr = temp2
+                    step3 = True
+            except IOError:
+                continue
+
+        if response == '' or addr == '':
+            print('client timeout')
+            return False, -1
+
+        if response [0] == 'ACK':
+            print('3- way - hand shake complite')
+            return True, addr
+        elif response [0] == 'NACK':
+            return  False, -1
+
+    else:
+        return False, -1
 
 
-def file_to_packets(file_name):
+
+
+
+def file_to_packets(file_name) -> list:
     # open file
     try:
         file = open("store/" + file_name, 'rb')
@@ -55,22 +111,132 @@ def file_to_packets(file_name):
         return
 
     # make all packets and add to the buffer
-    packets = []
+    packets_list = []
     seq_num = 0
-    while True:
-        data = file.read(PACKET_SIZE)
-        if not data:
-            break
-        packets.append(packet.make(seq_num, data))
-        seq_num += 1
-
-    window_size = set_window_size(len(packets))
-    file.close()
-
-    return packets, len(packets), window_size
+    while packet_data := file.read(MTU - 10):
+        packets_list.append(packet_data)
+    return packets_list
+    #     seq_num += 1
+    #
+    # window_size = set_window_size(len(packets_list))
+    # file.close()
+    #
+    # return packets_list, len(packets_list), window_size
 
 
-# send thread
+
+def file_sender_try(conn, clientaddr, packets_list):
+    conn.settimeout(TIMEOUT * 2)
+    index = 0
+    ack_list = []
+    for i in range(len(packets_list)):
+        ack_list.append(False)
+    acked_packet = 0
+    cwnd = 10
+    first_loss = False
+    halfway = True
+
+    while index != len(packets_list):
+        print('cwnd:', cwnd)
+
+        window_frame = min(index + cwnd, len(packets_list))
+        for k in range(index, window_frame):
+            if halfway:
+                if acked_packet == len(packets_list) / 2:
+                    k += 1
+                    halfway = False
+                    print('half way sending')
+                    # continue_req = transform
+
+                    try:
+                        result = conn.recv(4096).decode()
+                        if result == 'STOP':
+                            print('asked to stop.. im jast a server..')
+                            break
+                    except IOError:
+                        print('client time out')
+                        break
+                if not ack_list[k]:
+                    checksum = packet.calc_checksum(packets_list[k])
+                    packetk = packet.make(k, checksum, packets_list[k], cwnd)
+                    conn.sendto(packetk, clientaddr)
+
+            checker = index
+
+            for j in range (index, window_frame):
+                result, addr = receive_ack(conn)
+                threshhold = cwnd / 2
+
+                if result == 'TIMEOUT' or result[0] == 'TIMEOUT':
+                    if not first_loss:
+                        first_loss = True
+                        print('first loss event', result)
+                    cwnd = 10
+
+                elif result[0] == 'NACK':
+                    if not first_loss:
+                        first_loss = True
+                        print('first loss event', result)
+                    if cwnd > 10:
+                        cwnd -= 1
+
+                if result[0] == 'ACK':
+                    acked_packet += 1
+                    if not first_loss:
+                        cwnd *= 2
+                    else:
+                        if cwnd < threshhold:
+                            cwnd *= 2
+                        else:
+                            cwnd += 1
+                    if cwnd > 0.1 * len(packets_list):
+                        cwnd = int(0.1 * len(packets_list))
+                    ack_list[int(result[1])] = True
+
+                if result[0] == 'ACKALL':
+                    print('all packets received')
+                    index = len(packets_list)
+                    continue
+
+                if index != len(packets_list):
+                    for i in range (index, checker + window_frame + 1):
+                        if ack_list[i]:
+                            index += 1
+                        else:
+                            break
+            print('done')
+
+
+
+
+
+def UDP_thred(filename, port):
+    # conn.recv(BUFFER_SIZE).decode()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind('127.0.0.1', port)
+    sock.settimeout(60)
+    connection = sock
+
+    twh, addr = handshake(connection)
+    if twh:
+        print('3 way HS = sucsses')
+        packets = file_to_packets(file_name= filename)
+        for i in range (5):
+            connection.sendto(('length_' + str(len(packets))).encode(), addr)
+
+        time.sleep(1)
+        print('sent')
+        file_sender_try(connection, addr, packets)
+    else:
+        print('error 3 w HS')
+
+
+    sock.close()
+
+
+
+ #  send thread
 def send_file(conn, file_name):
     global mutex
     global base
@@ -99,7 +265,7 @@ def send_file(conn, file_name):
 
         # start timer
         if not send_timer.is_running():
-            send_timer.start(TIMEOUT_INTERVAL)
+            send_timer.start(TIMEOUT)
 
         # wait for ACK or time out
         while send_timer.is_running() and not send_timer.timeout:
@@ -123,26 +289,39 @@ def send_file(conn, file_name):
 
 
 def receive_ack(sock):
-    global mutex
+    try:
+        message, addr = sock.recvfrom(8192)
+        message = message.decode()
+        message = message.split('_', 1)
+        return message, addr
+    except TimeoutError:
+        return 'TIMEOUT', -1
+
+    # global mutex
+    # global base
+    # global send_timer
+    #
+    # print(sock)
+    # while True:
+    #     # sock.sendto(data.encode(), (RUDP_HOST, RUDP_PORT))
+    #     # sock.recvfrom(BUFFER_SIZE)
+    #     pak = sock.recvfrom(1024)
+    #     ack, g_data = packet.extract(pak)
+    #
+    #     # if got ACK for the first packet in-flight
+    #     print('got ACK', ack)
+    #     if ack >= base:
+    #         mutex.acquire()
+    #         base = (ack + 1) % 4
+    #         print('base update:', base)
+    #         send_timer.stop()
+    #         mutex.release()
+
+
+# set window size
+def set_window_size(num_packets):
     global base
-    global send_timer
-
-    print(sock)
-    while True:
-        # sock.sendto(data.encode(), (RUDP_HOST, RUDP_PORT))
-        # sock.recvfrom(BUFFER_SIZE)
-        pak = sock.recvfrom(1024)
-        ack, g_data = packet.extract(pak)
-
-        # if got ACK for the first packet in-flight
-        print('got ACK', ack)
-        if ack >= base:
-            mutex.acquire()
-            base = (ack + 1) % 4
-            print('base update:', base)
-            send_timer.stop()
-            mutex.release()
-
+    return min(WINDOW_SIZE, num_packets - base)
 
 def send_rudp_packet(sock, data):
     # for i in range(5):
@@ -253,7 +432,8 @@ def run_server(conn, addr):
                 if args[0] != "--tcp":
                     filename = args[0]
                     # foo(conn)
-                    send_file(conn, filename)
+                    # send_file(conn, filename)
+                    file_sender_try(conn=conn, clientaddr=addr, packets_list=file_to_packets(filename))
             elif command == 'UPLOAD':
                 pass
             else:
